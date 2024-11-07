138f5877c459396d9587b7d3b8c0741e7ae6d9c6
kirisuoc <erikcousillas@icloud.com>
2024-11-07 19:43:35 +0100

    Doing check errors functions

--- main.c
+++ main.c
@@ -3,74 +3,131 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecousill <ecousill@student.42.fr>          +#+  +:+       +#+        */
+/*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/07 12:48:08 by ecousill          #+#    #+#             */
-/*   Updated: 2024/11/07 13:43:09 by ecousill         ###   ########.fr       */
+/*   Created: 2024/11/07 18:30:07 by erikcousill       #+#    #+#             */
+/*   Updated: 2024/11/07 19:42:37 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minilibx-linux/mlx.h"
+#include "minilibx/mlx.h"
 #include <stddef.h>
 #include <stdlib.h>
-#include <X11/keysym.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-int close_window(void *param)
+#define MAX_LINE_LENGTH 1024
+
+int	read_map(const char *filename, char ***map)
 {
-    (void)param;
-    exit(0); // Termina el programa
+	int		fd;
+	int		bytes_read;
+	int		line;
+	char	buffer[MAX_LINE_LENGTH];
+
+	// Abrir el archivo .ber
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error al abrir el archivo");
+		exit(EXIT_FAILURE);
+	}
+
+	line = 0;
+	// Leer el archivo linea por linea
+	while ((bytes_read = read(fd, buffer, MAX_LINE_LENGTH)) > 0)
+	{
+		buffer[bytes_read] = '\0';
+
+		// Asignar memoria para cada linea y copiarla
+		(*map)[line] = strdup(buffer);
+		if (!map[line])
+		{
+			perror("Error al asignar memoria");
+			close(fd);
+			exit(EXIT_FAILURE);
+		}
+		line++;
+	}
+	close(fd);
+	return (0);
 }
 
-int close_on_esc(int keycode, void *param)
+// Terminar chequeando primera fila, última fila, primera columa y última columna
+void	check_walls(char **map)
 {
-    if (keycode == XK_Escape) // Verifica si la tecla presionada es ESC
-    {
-        exit(0); // Sale del programa
-    }
-    return (0);
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i] != NULL)
+	{
+		j = 0;
+		while (map[i][j] != '\0')
+		{
+			if (i == 0)
+			{
+				if (map[i][j] != 1)
+				{
+					perror("Hay algún muro exterior incorrecto");
+					exit (EXIT_FAILURE);
+				}
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
-int main()
+// Chequear que es rectangular --> Todas las filas tienen el mismo tamaño y lo mismo para las columnas
+void	check_shape(char **map)
 {
-    // Inicializar conexión a X
-    void *mlx = mlx_init();
-    void *win = mlx_new_window(mlx, 1366, 768, "Imagen");
-	void *win2 = mlx_new_window(mlx, 1366, 768, "Imagen");
 
-    // Crear una imagen en memoria de 800x600
-	void *img = mlx_new_image(mlx, 1366, 768);
+}
 
-	   // Declarar variables para almacenar la información de la imagen
-    int bpp;         // Bits por píxel
-    int size_line;   // Longitud de cada línea de la imagen en bytes
-    int endian;      // Orden de bytes: 0 para Little Endian, 1 para Big Endian
+// Mira que hay un camino válido desde el jugador hasta la salida, pasando por todos los items. Error si no
+void	check_valid_path(char **map)
+{
 
+}
+// Mira que solo hay un protagonista y (guarda dónde??). Error si no
+void	start_position(char **map)
+{
 
-	char *data = mlx_get_data_addr(img, &bpp, &size_line, &endian);
+}
+// Mira cuántos coleccionables hay y (guarda dónde están??). Error si no hay al menos 1 ?? En principio no.
+void	check_items(char **map)
+{
 
-     // Dibujar en la imagen (rellenar con un color, por ejemplo)
-    for (int y = 200; y < 600; y++)
-    {
-        for (int x = 0; x < 800; x++)
-        {
-            int pixel = (y * size_line) + (x * (bpp / 8));
-            data[pixel] = 255;    // Azul
-            data[pixel + 1] = 0;  // Verde
-            data[pixel + 2] = 0;  // Rojo
-        }
-    }
+}
 
+int	main(int argc, char **argv)
+{
+	char	**map;
+	int		i;
 
-	mlx_put_image_to_window(mlx, win, img, 0, 0);
-	mlx_put_image_to_window(mlx, win2, img, 0, 0);
-
-	// Captura el evento de cerrar la ventana
-    mlx_hook(win, 17, 0, close_window, NULL);
-	mlx_hook(win2, 17, 0, close_window, NULL);
-
-    mlx_key_hook(win, close_on_esc, NULL);
-
-    // Esperar y mantener la ventana abierta
-    mlx_loop(mlx);
-    return (0);
+	if (argc != 2)
+	{
+		printf("Uso: %s <archivo.ber>\n", argv[0]);
+		return (1);
+	}
+	map = malloc(sizeof(char *) * 1024);
+	if (read_map(argv[1], &map) == -1)
+	{
+		perror("Error al leer el mapa");
+		return (2);
+	}
+	check_walls(map);
+		return (1);
+	i = 0;
+	while (map[i] != NULL)
+	{
+		printf("%s", map[i]);
+		free(map[i]);	// Liberar cada línea después de imprimirla
+		i++;
+	}
+	free(map);
+	return (0);
 }
---
+++ main2.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 12:48:08 by ecousill          #+#    #+#             */
+/*   Updated: 2024/11/07 16:48:27 by erikcousill      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minilibx/mlx.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+int close_window(void *param)
+{
+    (void)param;
+    exit(0); // Termina el programa
+}
+
+/* int close_on_esc(int keycode, void *param)
+{
+    if (keycode == XK_Escape) // Verifica si la tecla presionada es ESC
+    {
+        exit(0); // Sale del programa
+    }
+    return (0);
+} */
+
+int main()
+{
+    // Inicializar conexión a X
+    void *mlx = mlx_init();
+    void *win = mlx_new_window(mlx, 1366, 768, "Imagen");
+	//void *win2 = mlx_new_window(mlx, 1366, 768, "Imagen");
+
+    // Crear una imagen en memoria de 800x600
+	void *img = mlx_new_image(mlx, 1366, 768);
+
+	   // Declarar variables para almacenar la información de la imagen
+    int bpp;         // Bits por píxel
+    int size_line;   // Longitud de cada línea de la imagen en bytes
+    int endian;      // Orden de bytes: 0 para Little Endian, 1 para Big Endian
+
+	unsigned char *data = (unsigned char *)mlx_get_data_addr(img, &bpp, &size_line, &endian);
+
+
+     // Dibujar en la imagen (rellenar con un color, por ejemplo)
+    for (int y = 200; y < 600; y++)
+    {
+        for (int x = 0; x < 800; x++)
+        {
+            int pixel = (y * size_line) + (x * (bpp / 8));
+            data[pixel] = 255;    // Azul
+            data[pixel + 1] = 0;  // Verde
+            data[pixel + 2] = 0;  // Rojo
+        }
+    }
+
+
+	mlx_put_image_to_window(mlx, win, img, 0, 0);
+	//mlx_put_image_to_window(mlx, win2, img, 0, 0);
+
+	// Captura el evento de cerrar la ventana
+    mlx_hook(win, 17, 0, close_window, NULL);
+	//mlx_hook(win2, 17, 0, close_window, NULL);
+
+    //mlx_key_hook(win, close_on_esc, NULL);
+
+    // Esperar y mantener la ventana abierta
+    mlx_loop(mlx);
+    return (0);
+}
