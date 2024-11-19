316f5e82921fe266d18f106c8e36129d9f1c0c18
Erik Cousillas Fernandez <ecousill@c3r4s6.42urduliz.com>
2024-11-19 15:00:47 +0100

    Fix ASDW Linux

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: ecousill <ecousill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 10:48:20 by ecousill          #+#    #+#             */
-/*   Updated: 2024/11/14 12:33:18 by ecousill         ###   ########.fr       */
+/*   Updated: 2024/11/19 15:00:05 by ecousill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,16 @@ int	close_window(t_vars *vars)
 
 int key_handler(int keycode, t_vars *vars)
 {
+	printf("%d\n", keycode);
 	if (keycode == 53 || keycode == 65307)
 		close_window(vars);
-	else if((keycode == 2 || keycode == 124 || keycode == 65363) && !vars->won)
+	else if((keycode == 2 || keycode == 100 || keycode == 124 || keycode == 65363) && !vars->won)
 		update_player_position(vars, (t_point){vars->player.pos.px_x + 1, vars->player.pos.px_y});
-	else if((keycode == 0 || keycode == 123 || keycode == 65361) && !vars->won)
+	else if((keycode == 0 || keycode == 97 || keycode == 123 || keycode == 65361) && !vars->won)
 		update_player_position(vars, (t_point){vars->player.pos.px_x - 1, vars->player.pos.px_y});
-	else if((keycode == 13 || keycode == 126 || keycode == 65362) && !vars->won)
+	else if((keycode == 13 || keycode == 119 || keycode == 126 || keycode == 65362) && !vars->won)
 		update_player_position(vars, (t_point){vars->player.pos.px_x, vars->player.pos.px_y - 1});
-	else if((keycode == 1 || keycode == 125 || keycode == 65364) && !vars->won)
+	else if((keycode == 1 || keycode == 115 || keycode == 125 || keycode == 65364) && !vars->won)
 		update_player_position(vars, (t_point){vars->player.pos.px_x, vars->player.pos.px_y + 1});
 	return (0);
 }
